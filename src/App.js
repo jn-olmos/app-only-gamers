849eb43d2182f53af7dbb4bf3d0c0ff3e8340fb4
jn.olmos <romero.nico18@gmail.com>
2022-06-26 18:59:06 -0300

    estilos bases y reset agregados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css'
+import './scss/components/_app.scss';
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<h1>title</h1>
+			<h2>title</h2>
+			<h3>title</h3>
+			<h4>title</h4>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, incidunt sequi.
+				Eos, debitis, officia consequuntur itaque voluptatum perspiciatis, veniam nihil odio
+				iure laboriosam autem corrupti consectetur fugiat recusandae numquam consequatur.
+			</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
